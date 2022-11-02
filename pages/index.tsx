5e47a70b0f9485af0e6e4cab21c9657bd0b97224
Zachary Hansen <zachary.r.hansen@gmail.com>
2022-11-02 13:44:06 -0700

    format proj

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,8 @@ import styles from "../styles/Home.module.css";
 import { Fragment, useEffect, useState } from "react";
 import { IAPODQueryParams, IAPODResponse } from "../models/apiModels";
 import { getAPODDAL } from "../apodDAL";
-import APODModal from "../components/modals/APODModal";
 import { Box } from "@mui/material";
 import moment from "moment";
-import Loading from "../components/Loading";
-import ImageListing from "../components/ImageListing";
 
 const Home = () => {
   const [response, setResponse] = useState<IAPODResponse[]>();
@@ -21,39 +18,13 @@ const Home = () => {
 
   useEffect(() => getAPOD({ date: date.format("YYYY-MM-DD") }), [date]);
 
-  const handleDateChange = (date?: moment.Moment | null) => {
-    if (date) {
-      setDate(date);
-    }
-  };
-
-  const handleRandomize = () => {
-    getAPOD({ count: 10 });
-  };
-
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         {!response ? (
-          <Loading />
+          <text>loading... implement a nice loader</text>
         ) : (
-          <Fragment>
-            <ImageListing items={response} />
-            <Box
-              sx={{
-                "& > :not(style)": { m: 1 },
-                position: "absolute",
-                top: 0,
-                right: 0,
-              }}
-            >
-              <APODModal
-                onDateChange={handleDateChange}
-                date={date}
-                onRandomize={handleRandomize}
-              />
-            </Box>
-          </Fragment>
+          <Fragment>{JSON.stringify(response)}</Fragment>
         )}
       </main>
     </div>
